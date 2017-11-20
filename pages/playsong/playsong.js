70a81d800d75273be349c6e1c763dd44062bea5e
fxc <fxc>
2017-11-20 15:24:26 +0800

    first commit

---
+++ pages/playsong/playsong.js
@@ -0,0 +1,77 @@
+// pages/playsong/playsong.js
+var app=getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(app)
+    //把数数存到data里面
+    var data = app.globalData.playlist;
+    this.setData({
+      musiclist: data
+    })
+    
+    //http://ws.stream.qqmusic.qq.com/C100' + songmid + '.m4a?fromtag=38
+    wx.playBackgroundAudio({
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/C100' + data.songmid+'.m4a?fromtag=38'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
